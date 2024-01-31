f1fd9201e0b1b7dab935a9050ece700b45e6cc9a
Alberto Junior <albertojunior.05@gmail.com>
2024-01-30 21:22:39 -0300

    adicionados programas utilitarios

---
+++ InstallApps.ps1
@@ -0,0 +1,8 @@
+choco install winrar -y --ignore-checksums
+choco install googlechrome -y --ignore-checksums
+choco install adobereader -y --ignore-checksums
+choco install jre8 -y --ignore-checksums
+choco install python3 -y --ignore-checksums
+choco install git -y --ignore-checksums
+
+
